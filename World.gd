631b52823911dfaab056fb9b2208af7aad9f5b98
nuzcraft <nuzcraft@gmail.com>
2022-09-11 10:50:37 -0500

    added hidden and secret tilemaps with programmatic collision

--- World.gd
+++ World.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 onready var tileMap := $TileMap
+onready var hiddenTilemap := $Hidden_Tilemap
+onready var secretsTilemap := $Secrets_Tilemap
+onready var player := $Player
 
 const greyScale = preload("res://TileMap/Greyscale.tres")
 const GRASS = 0
@@ -14,6 +17,7 @@ func _ready():
 	Events.connect("restart_game", self, "on_restart_game")
 	Events.connect("ring_on", self, "on_ring_on")
 	Events.connect("ring_off", self, "on_ring_off")
+	hiddenTilemap.visible = false
 
 func on_win_game():
 	get_tree().change_scene("res://WinScreen.tscn")
@@ -26,9 +30,17 @@ func on_ring_on():
 	tileMap.tile_set.tile_set_material(MUD, greyScale)
 	tileMap.tile_set.tile_set_material(SNOW, greyScale)
 	tileMap.tile_set.tile_set_material(ASH, greyScale)
+	hiddenTilemap.visible = true
+	secretsTilemap.visible = false
+	player.set_collision_mask_bit(4, true)
+	player.set_collision_mask_bit(5, false)
 	
 func on_ring_off():
 	tileMap.tile_set.tile_set_material(GRASS, null)
 	tileMap.tile_set.tile_set_material(MUD, null)
 	tileMap.tile_set.tile_set_material(SNOW, null)
 	tileMap.tile_set.tile_set_material(ASH, null)
+	hiddenTilemap.visible = false
+	secretsTilemap.visible = true
+	player.set_collision_mask_bit(4, false)
+	player.set_collision_mask_bit(5, true)
