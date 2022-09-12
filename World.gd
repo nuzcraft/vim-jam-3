fe044e99241278c021432f2f9b7b8fecefc51756
nuzcraft <nuzcraft@gmail.com>
2022-09-11 20:33:49 -0500

    still working on shaders

--- World.gd
+++ World.gd
@@ -6,10 +6,6 @@ onready var secretsTilemap := $Secrets_Tilemap
 onready var player := $Player
 
 const greyScale = preload("res://TileMap/Greyscale.tres")
-const GRASS = 0
-const MUD = 1
-const SNOW = 2
-const ASH = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,6 +14,7 @@ func _ready():
 	Events.connect("ring_on", self, "on_ring_on")
 	Events.connect("ring_off", self, "on_ring_off")
 	hiddenTilemap.visible = false
+	hiddenTilemap.disappear()
 
 func on_win_game():
 	get_tree().change_scene("res://WinScreen.tscn")
@@ -26,18 +23,16 @@ func on_restart_game():
 	get_tree().reload_current_scene()
 	
 func on_ring_on():
-#	tileMap.
+	tileMap.goGreyscale()
 	hiddenTilemap.visible = true
-	secretsTilemap.visible = false
+	hiddenTilemap.reappear()
+	secretsTilemap.disappear()
 	player.set_collision_mask_bit(4, true)
 	player.set_collision_mask_bit(5, false)
 	
 func on_ring_off():
-	tileMap.tile_set.tile_set_material(GRASS, null)
-	tileMap.tile_set.tile_set_material(MUD, null)
-	tileMap.tile_set.tile_set_material(SNOW, null)
-	tileMap.tile_set.tile_set_material(ASH, null)
-	hiddenTilemap.visible = false
-	secretsTilemap.visible = true
+	tileMap.returnFromGreyscale()
+	hiddenTilemap.disappear()
+	secretsTilemap.reappear()
 	player.set_collision_mask_bit(4, false)
 	player.set_collision_mask_bit(5, true)
